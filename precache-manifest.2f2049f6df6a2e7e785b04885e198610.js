73bbf04fdeffea3c0e6065351e3210d411da8219
Edwin Rivera <waehake@gmail.com>
2019-02-26 12:00:41 -0500

    Updates

---
+++ precache-manifest.2f2049f6df6a2e7e785b04885e198610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9506dd8daa25b6f4d4c1a3d36417e27",
+    "url": "/critical-thinking/static/media/mountains.a9506dd8.png"
+  },
+  {
+    "revision": "0d347ba775129b13b69b",
+    "url": "/critical-thinking/static/js/runtime~main.0d347ba7.js"
+  },
+  {
+    "revision": "3db9bcc53566dafa1531",
+    "url": "/critical-thinking/static/js/main.3db9bcc5.chunk.js"
+  },
+  {
+    "revision": "f00e2c75825cf71a90c8",
+    "url": "/critical-thinking/static/js/2.f00e2c75.chunk.js"
+  },
+  {
+    "revision": "3db9bcc53566dafa1531",
+    "url": "/critical-thinking/static/css/main.3fc5c878.chunk.css"
+  },
+  {
+    "revision": "2bba65c21c00dca90693ba42a30c126c",
+    "url": "/critical-thinking/index.html"
+  }
+];
